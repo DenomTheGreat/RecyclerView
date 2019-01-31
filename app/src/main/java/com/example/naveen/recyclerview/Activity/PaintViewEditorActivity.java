52ff994e57fbe25b9bab25b202d09e56a8c47f1d
Naveen Bansal <nbansal1@cs.iitr.ac.in>
2019-01-31 22:29:50 +0530

    master

--- app/src/main/java/com/example/naveen/recyclerview/Activity/PaintViewEditorActivity.java
+++ app/src/main/java/com/example/naveen/recyclerview/Activity/PaintViewEditorActivity.java
@@ -2,6 +2,7 @@ package com.example.naveen.recyclerview.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -26,6 +27,17 @@ public class PaintViewEditorActivity extends AppCompatActivity {
             paintView.saveView();
             }
         });
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
